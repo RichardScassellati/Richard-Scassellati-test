51d204b5657ac125cd613c6ee0f9979559f39842
Richard Scassellati <rn.scassellati@setonhill.edu>
2020-01-29 09:37:28 -0500

    Initial Commit

---
+++ Storyboard Intro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Storyboard Intro
+//
+//  Created by Richard Scassellati on 1/29/20.
+//  Copyright © 2020 Smecko Geck Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
